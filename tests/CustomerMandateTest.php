56dded53bae29b2e13d64402917c4b6df5f60170
Maarten de Boer <maarten@cloudstek.nl>
2016-08-20 22:40:05 +0200

    Add customer mandate tests

--- tests/CustomerMandateTest.php
+++ tests/CustomerMandateTest.php
@@ -45,7 +45,7 @@ class CustomerMandateTest extends ResourceTestCase
      */
     public function testGetCustomerMandates()
     {
-        // Prepare a list of mandates for John Doe
+        // Prepare a list of mandates
         $mandateListMock = [];
 
         for ($i = 0; $i <= 15; $i++) {
@@ -76,6 +76,57 @@ class CustomerMandateTest extends ResourceTestCase
 
         // Check all mandates
         $this->assertMandates($mandates, $mandateListMock);
+    }
+
+    /**
+     * Create customer mandate
+     */
+    public function testCreateCustomerMandate()
+    {
+        // Mock the mandate
+        $mandateMock = $this->getCustomerMandate();
+
+        // Mock the customer
+        $customerMock = $this->getCustomer();
+
+        // Create API instance
+        $api = new Mollie('test_testapikey');
+
+        // Mock the request
+        $requestMock = $this->createMock(Request::class);
+
+        $requestMock
+            ->expects($this->once())
+            ->method('post')
+            ->with(
+                $this->equalTo("/customers/{$customerMock->id}/mandates"),
+                $this->equalTo([
+                    'method' => 'directdebit',
+                    'consumerName' => $mandateMock->details->consumerName,
+                    'consumerAccount' => $mandateMock->details->consumerAccount,
+                    'consumerBic' => $mandateMock->details->consumerBic,
+                    'signatureDate' => $mandateMock->details->signatureDate,
+                    'mandateReference' => $mandateMock->details->mandateReference
+                ])
+            )
+            ->will($this->returnValue($mandateMock));
+
+        // Set request handler
+        $api->request = $requestMock;
+
+        // Create mandate
+        $mandate = $api->customer($customerMock->id)->mandate()->create(
+            $mandateMock->details->consumerName,
+            $mandateMock->details->consumerAccount,
+            [
+                'bic' => $mandateMock->details->consumerBic,
+                'signatureDate' => new \DateTime($mandateMock->details->signatureDate),
+                'reference' => $mandateMock->details->mandateReference
+            ]
+        );
+
+        // Check if we have the correct mandate
+        $this->assertMandate($mandate, $mandateMock);
     }
 
     /**
@@ -133,6 +184,84 @@ class CustomerMandateTest extends ResourceTestCase
         $mandate->status = "invalid";
         $this->assertTrue($mandate->isInvalid());
         $this->assertFalse($mandate->isValid());
+    }
+
+    /**
+     * Check if a list of mandates has a valid mandate
+     */
+    public function testCustomerMandateHasValid()
+    {
+        // Prepare a list of mandates
+        $mandateListMock = [];
+
+        for ($i = 0; $i <= 15; $i++) {
+            $mandate = $this->getMandate("invalid");
+            $mandate->id .= "_{$i}";   // mdt_test_1
+
+            // Add mandate to list
+            $mandateListMock[] = $mandate;
+        }
+
+        // Mock the customer
+        $customerMock = $this->getCustomer();
+
+        // Create API instance
+        $api = new Mollie('test_testapikey');
+
+        // Mock the request handler
+        $requestMock = $this->getMultiPageRequestMock($api, $mandateListMock, "/customer/{$customerMock->id}/mandates");
+
+        // Set request handler
+        $api->request = $requestMock;
+
+        // Mandate resource
+        $customerMandate = $api->customer($customerMock->id)->mandate();
+
+        // Make sure no valid mandates are found
+        $this->assertFalse($customerMandate->hasValid());
+
+        // Set a mandate to valid and check again
+        $mandateListMock[10]->status = 'valid';
+        $this->assertTrue($customerMandate->hasValid());
+    }
+
+    /**
+     * Create customer mandate with invalid signature date
+     *
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Argument signatureDate must be of type DateTime
+     */
+    public function testCreateCustomerMandateInvalidSignatureDate()
+    {
+        // Mock the mandate
+        $mandateMock = $this->getCustomerMandate();
+
+        // Create API instance
+        $api = new Mollie('test_testapikey');
+
+        // Mock the request
+        $requestMock = $this->createMock(Request::class);
+
+        $requestMock
+            ->expects($this->never())
+            ->method('post');
+
+        // Set request handler
+        $api->request = $requestMock;
+
+        // Create mandate
+        $mandate = $api->customer('cst_test')->mandate()->create(
+            $mandateMock->details->consumerName,
+            $mandateMock->details->consumerAccount,
+            [
+                'bic' => $mandateMock->details->consumerBic,
+                'signatureDate' => 'lalala', // Invalid date
+                'reference' => $mandateMock->details->mandateReference
+            ]
+        );
+
+        // Check if we have the correct mandate
+        $this->assertMandate($mandate, $mandateMock);
     }
 
     /**
